5903304e93b4e4b41c5e1c76c1272833d76cb9fe
RukeshShrestha <rukesh.shrestha11@gmail.com>
2023-10-13 21:46:46 +0545

    Added Swagger API and morgan debugger

--- server.js
+++ server.js
@@ -3,14 +3,21 @@ const app = express();
 import "dotenv/config";
 import connectDB from "./config/dbConnector.js";
 import productRouter from "./routes/productRoutes.js";
-
+import morgan from "morgan";
+import swaggerui from "swagger-ui-express";
+import yaml from "yamljs";
 const PORT = process.env.PORT;
 
 //Connecting Database
 connectDB();
 
+//Configuring the Swagger API Documentation
+const swaggerDocument = yaml.load("./swagger/swaggerAPI.yml");
+
 //Middleware
 app.use(express.json());
+app.use("/api-docs", swaggerui.serve, swaggerui.setup(swaggerDocument));
+app.use(morgan("dev"));
 app.use("/api/products", productRouter);
 
 app.listen(PORT, () =>
